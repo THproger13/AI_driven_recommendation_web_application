025b59643d2a95eacfc3a306a66babcffa265e62
THproger13 <thphysics@naver.com>
2023-08-12 17:11:21 +0900

    카페 추천 웹사이트

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
 import React from 'react';
-import './Header.css'; // 스타일 파일을 불러옴
+import './Header.css';
 
 const Header = () => {
   return (
@@ -10,15 +10,14 @@ const Header = () => {
           <source src="/커피2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Row className="align-items-center">
-              <Col className="text-left">좌측 상단 내용</Col>
-              <Col className="text-center">중앙 내용</Col>
-              <Col className="text-right">우측 상단 내용</Col>
-            </Row>
-          </Container>
-        </Navbar>
+        
+        <Container fluid className="content-container">
+          <Row className="align-items-center justify-content-between">
+            <Col xs={12} md={4} className="left-content">좌측 상단 내용</Col>
+            <Col xs={12} md={4} className="center-content">중앙 내용</Col>
+            <Col xs={12} md={4} className="right-content">우측 상단 내용</Col>
+          </Row>
+        </Container>
       </div>
     </header>
   );
